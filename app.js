d82e5fb05789cb61fdc1adb5aaacbb7dd595f189
subender <subender.io@outlook.com>
2022-03-13 01:42:56 +0530

    Created Blocks & User.

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
-console.log("test");
+const grid = document.querySelector(".grid");
+const blockWidth = 13;
+const blockHeight = 3;
+const boardWidth = 72;
+const userStartPosition = [29, 1];
+let userCurrentPosition = userStartPosition;
+
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.bottomRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+
+const blocksArray = [
+  new Block(1, 46),
+  new Block(15, 46),
+  new Block(29, 46),
+  new Block(43, 46),
+  new Block(58, 46),
+  new Block(1, 42),
+  new Block(15, 42),
+  new Block(29, 42),
+  new Block(43, 42),
+  new Block(58, 42),
+  new Block(1, 38),
+  new Block(15, 38),
+  new Block(29, 38),
+  new Block(43, 38),
+  new Block(58, 38),
+];
+
+function createBlocks() {
+  for (let i = 0; i < blocksArray.length; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.left = blocksArray[i].bottomLeft[0] + "rem";
+    block.style.bottom = blocksArray[i].bottomLeft[1] + "rem";
+    grid.appendChild(block);
+  }
+}
+
+createBlocks();
+
+//Draw User
+
+function drawUser() {
+  user.style.left = userCurrentPosition[0] + "rem";
+  user.style.bottom = userCurrentPosition[1] + "rem";
+}
+
+//Creating user
+
+const user = document.createElement("div");
+user.classList.add("user");
+drawUser();
+grid.appendChild(user);
